0a73453c5a081d0af82312396f443e5d514f4ab4
Venugopal <venu.gopi148@gmail.com>
2018-03-24 17:05:14 +0530

    Angular project setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  selector: 'my-app',
+  template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent {
-  title = "My first CodeSandbox Project";
-}
+export class AppComponent  { name = 'Angular'; }
